1eea2c5803f3df25dfd859d96b9002f7d13c2f8f
mohamedoraibi <mohamed.b.oraibi@gmail.com>
2021-02-02 00:08:01 +0300

    - Bug Fixes

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -131,7 +131,7 @@
                 <!-- Newsletter -->
                 <div class="col-xl-4 col-lg-4 col-md-12">
                     <h3><i class="icon-feather-mail"></i> Sign Up For a Newsletter</h3>
-                    <p>Weekly breaking news, analysis and cutting edge advices on job searching.</p>
+                    <p>Weekly breaking news of DesignChi</p>
                     <form action="#" method="get" class="newsletter">
                         <input type="text" name="fname" placeholder="Enter your email address">
                         <button type="submit"><i class="icon-feather-arrow-right"></i></button>
